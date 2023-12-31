efc0b35deb5626c946027de22d99943b87a7711e
sorena <mdn1376@gmail.com>
2023-12-31 19:58:42 +0330

    added search

--- instagram.py
+++ instagram.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -16,4 +18,13 @@ username.send_keys(input('enter username: '))
 password = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input')
 password.send_keys(input('enter password: ') + Keys.ENTER)
 
+search_list = input('enter your search items(separate with ,): ').split(',')
+
+for item in search_list:
+    if item.startswith('#'):
+        driver.get(f'https://www.instagram.com/explore/tags/{item[1:]}/')
+    else:
+        driver.get(f'https://www.instagram.com/{item}/')
+
 input('press enter to exit: ')
+driver.quit()
